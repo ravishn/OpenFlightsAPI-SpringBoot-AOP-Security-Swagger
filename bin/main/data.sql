48be110e97afe3195863e77b6f9023785cad12c6
ravishn <ravish.nagaraj@gmail.com>
2020-02-24 16:54:57 +1100

    update local branch

--- bin/main/data.sql
+++ bin/main/data.sql
@@ -6,7 +6,7 @@ insert into ROUTES (select * from CSVREAD('C:\VisualStudioCodeWorkspace\demo\Ope
 insert into AIRLINES(select * from CSVREAD('C:\VisualStudioCodeWorkspace\demo\OpenFlightsAPI-SpringBoot-AOP-Security-Swagger-master\src\main\resources\airlines.dat'));
 
 
--- use this for building and running the application inside Docker container
+-- IMPORTANT: use this for building and running the application inside Docker container
 -- insert into AIRPORTS (select * from CSVREAD('src\main\resources\airports.dat'));
 
 -- insert into ROUTES (select * from CSVREAD('src\main\resources\routes.dat'));
