d2ceb406a44ab8f0924be55fe0981151d38d1202
ravishn <ravish.nagaraj@gmail.com>
2020-02-24 15:24:54 +1100

    local branch for running API locally

--- bin/main/data.sql
+++ bin/main/data.sql
@@ -1,5 +1,14 @@
-insert into AIRPORTS (select * from CSVREAD('src\main\resources\airports.dat'));
+-- use this for running locally
+insert into AIRPORTS (select * from CSVREAD('C:\VisualStudioCodeWorkspace\demo\OpenFlightsAPI-SpringBoot-AOP-Security-Swagger-master\src\main\resources\airports.dat'));
 
-insert into ROUTES (select * from CSVREAD('src\main\resources\routes.dat'));
+insert into ROUTES (select * from CSVREAD('C:\VisualStudioCodeWorkspace\demo\OpenFlightsAPI-SpringBoot-AOP-Security-Swagger-master\src\main\resources\routes.dat'));
 
-insert into AIRLINES(select * from CSVREAD('src\main\resources\airlines.dat'));
+insert into AIRLINES(select * from CSVREAD('C:\VisualStudioCodeWorkspace\demo\OpenFlightsAPI-SpringBoot-AOP-Security-Swagger-master\src\main\resources\airlines.dat'));
+
+
+-- use this for building and running the application inside Docker container
+-- insert into AIRPORTS (select * from CSVREAD('src\main\resources\airports.dat'));
+
+-- insert into ROUTES (select * from CSVREAD('src\main\resources\routes.dat'));
+
+-- insert into AIRLINES(select * from CSVREAD('src\main\resources\airlines.dat'));
